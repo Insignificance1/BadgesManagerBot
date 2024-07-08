8795b28cd4477a1791170def337656d7cd9649b1
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2024-07-08 12:21:50 +0500

    Пум

--- model/detection.py
+++ model/detection.py
@@ -67,10 +67,6 @@ class Detector:
             # Добавление дополнительных отступов вокруг минимального квадрата
             final_image = self.add_padding(min_square_image, padding)
             final_image.save(output_image_path)
-        shutil.rmtree('../Photo/cut')
-        shutil.rmtree('../Photo/auto_rotated')
-        shutil.rmtree('../Photo/original')
-
         return num_objects
 
     def add_padding(self, image, padding):
